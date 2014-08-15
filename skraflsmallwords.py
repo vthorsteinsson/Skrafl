84e2a149b32a6240e17e9f41d94bd160e5f98925
vthorsteinsson <vt@extrada.com>
2014-08-15 15:13:26 +0000

    Updated code to prepare small words file
    Creates a sorted word file in UTF-8 from the small words list in
    ISO8859-1

--- skraflsmallwords.py
+++ skraflsmallwords.py
@@ -1,22 +1,52 @@
-""" Small word processor for the Skrafl application """
+# -*- coding: utf-8 -*-
+""" Small word processor for the Skrafl application
 
+    This code is written for Python 3
+
+"""
+
+import codecs
 import locale
-import encodings.iso8859_1
+import functools
+import os.path
 
-#locale.setlocale(locale.LC_ALL, 'Icelandic_Iceland') # Use Icelandic locale for string operations
+def run():
+    """ Read input file in iso8859-1, sort it using Icelandic sort order and output it in utf-8 """
 
-infile_name = 'c:\\users\\user\\dropbox\\BIN\\smaord.txt'
-outfile_name = 'c:\\users\\user\\dropbox\\BIN\\smaordalisti.txt'
-#encoder = encodings.iso8859_1.Codec();
-#banned = encoder.encode('.-/ABCDEFGHIJKLMNOPQRSTUVWXYZÞÆÖÐÁÉÍÓÚÝ')
+    print("Beygingarlýsing íslensks nútímamáls, smáorð -> skraflgrunnur")
 
-print ("Beygingarlýsing íslensks nútímamáls, smáorð -> skraflgrunnur")
+    locale.setlocale(locale.LC_ALL, 'isl') # Use Icelandic locale for string operations
 
-with open(infile_name, mode='r', encoding='iso8859-1') as fin :
-    with open(outfile_name, mode='w', encoding='iso8859-1') as fout :
-        for line in fin :
-            # The lines end with a newline character
-            if len(line) > 1 :
-                fout.write(line.split(None, 1)[0] + '\n')
+    relpath = "resources"
+    fname = "smaord"
 
-                
+    infile_name = os.path.abspath(os.path.join(relpath, fname + ".txt"))
+    outfile_name = os.path.abspath(os.path.join(relpath, fname + ".sorted.txt"))
+
+    print("Inntak úr {0}".format(infile_name))
+    print("Úttak í {0}".format(outfile_name))
+
+    keyfunc = functools.cmp_to_key(locale.strcoll)
+    wordlist = []
+
+    with codecs.open(infile_name, mode='r', encoding='iso8859-1') as fin:
+        for line in fin:
+            if not line:
+                continue
+            if line[:-1] == '\n':
+                line = line[0:-1]
+            elif line[:-2] == '\r\n':
+                line = line[0:-2]
+            if not line:
+                continue
+            wordlist.append(line.split(' ')[0])
+
+    print("Raða {0} orðum".format(len(wordlist)))
+    wordlist.sort(key=keyfunc)
+
+    print("Skrifa úttak")
+    with codecs.open(outfile_name, mode='w', encoding='utf-8') as fout :
+        for word in wordlist:
+            fout.write(word + '\n')
+
+    print("Vinnslu lokið")
