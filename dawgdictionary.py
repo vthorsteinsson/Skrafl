8d905203b3083550a109e7c1dfe8774ec9a7a108
vthorsteinsson <vt@extrada.com>
2014-08-20 16:17:20 +0000

    Added DawgDictionary.find_matches()
    Pattern matching implemented. DawgTester.py separated out of
    DawgDictionary.py

--- dawgdictionary.py
+++ dawgdictionary.py
@@ -8,14 +8,34 @@ DawgDictionary uses a Directed Acyclic Word Graph (DAWG) internally
 to store a large set of words in an efficient structure in terms
 of storage and speed.
 
+The class supports three main query functions:
+
+DawgDictionary.find(word)
+    Returns True if the word is found in the dictionary, or False if not.
+    The __contains__ operator is supported, so "'myword' in dawgdict" also works.
+
+DawgDictionary.find_matches(pattern)
+    Returns a list of words that match the pattern. The pattern can contain
+    wildcards ('?'). For example, result = dawgdict.find_matches("ex???") returns
+    a list of all 5-letter words starting with "ex".
+
+DawgDictionary.find_permutations(rack)
+    Returns a list of all permutations of the given rack, i.e. valid words
+    consisting of one or more letters from the rack in various orders.
+    The rack may contain wildcards ('?'). For example, result = dawgdict.find_permutations("se?")
+    returns a list of all words from 1 to 3 characters that can be constructed from
+    the letters "s" and "e" and any one additional letter.
+
 See also comments in dawgbuilder.py
+
+Test code for this module is found in dawgtester.py
 
 """
 
 import os
 import codecs
 
-from languages import Icelandic as Icelandic
+from languages import Icelandic
 
 class DawgDictionary:
 
@@ -87,6 +107,10 @@ class DawgDictionary:
                 if line:
                     self._parse_and_add(line)
 
+    def num_nodes(self):
+        """ Return a count of unique nodes in the DAWG """
+        return 0 if self._nodes is None else len(self._nodes)
+
     def _nav_from_node(self, node, word, index):
         """ Starting from a given node, navigate the graph using the word and an index into it """
         if index >= len(word):
@@ -128,6 +152,8 @@ class DawgDictionary:
 
     def find(self, word):
         """ Look for a word in the graph, returning True if it is found or False if not """
+        if not word:
+            return False
         if self._nodes is None:
             # Must load the graph before searching
             return False
@@ -137,11 +163,85 @@ class DawgDictionary:
             return False
         return self._nav_from_node(root, word, 0)
 
-    def num_nodes(self):
-        """ Return a count of unique nodes in the DAWG """
-        return 0 if self._nodes is None else len(self._nodes)
+    def __contains__(self, word):
+        """ Enable simple lookup syntax: "word" in dawgdict """
+        return self.find(word)
 
-    def _append_perm_from_node(self, node, matched, rack, permlist):
+    def _match_from_node(self, node, pattern, index, matched, matchlist):
+        """ Starting from a given node, navigate the graph attempting to
+            match the pattern from the index position """
+        if node is None:
+            # Nothing more to do (we assume that a match has already been recorded)
+            return
+        assert pattern is not None
+        assert index < len(pattern)
+        # Go through the edges of this node and follow all that match a
+        # letter in the rack (which can be '?', matching all edges)
+        chmatch = pattern[index]
+        wildcard = (chmatch == u'?')
+        for prefix, nextnode in node.edges.items():
+            if wildcard or (prefix[0] == chmatch):
+                # This edge is a candidate: navigate through it
+                self._match_from_edge(pattern, index, prefix, nextnode, matched, matchlist)
+                if not wildcard:
+                    # No more than one edge can match the pattern if it's not a wildcard
+                    return
+
+    def _match_from_edge(self, pattern, index, prefix, nextnode, matched, matchlist):
+        """ The letter at pattern[index] matches the prefix of this edge """
+        assert index < len(pattern)
+        assert (pattern[index] == u'?') or (pattern[index] == prefix[0])
+        lenp = len(prefix)
+        j = 0
+        while j < lenp:
+            prefj = prefix[j]
+            if (pattern[index] != u'?') and (pattern[index] != prefj):
+                # Not a wildcard and the prefix does not match the pattern: we're done
+                return
+            # So far, we have a match
+            matched += prefj
+            index += 1
+            j += 1
+            final = False
+            # Check whether the next prefix character is a vertical bar, denoting finality
+            if j < lenp and prefix[j] == '|':
+                final = True
+                j += 1
+            if index >= len(pattern):
+                # The pattern is hereby exhausted:
+                # We have a match if this was a final char in the prefix,
+                # or if the prefix is exhausted and the next node is terminal
+                if final or ((j >= lenp) and ((nextnode is None) or nextnode.final)):
+                    matchlist.append(matched)
+                return
+        # This edge is exchausted and fully matched: proceed to the following node,
+        # if we have one
+        if nextnode:
+            assert index < len(pattern)
+            self._match_from_node(nextnode, pattern, index, matched, matchlist)
+
+    def find_matches(self, pattern):
+        """ Returns a list of words matching a pattern.
+            The pattern contains characters and '?'-signs denoting wildcards.
+            Characters are matched exactly, while the wildcards match any character.
+        """
+        matchlist = []
+        if self._nodes is None:
+            # No graph: no matches
+            return matchlist
+        if not pattern:
+            # Nothing to search for
+            return matchlist
+        root = self._nodes[0] # Start at the root
+        if root is None:
+            # No root: no matches
+            return matchlist
+        self._match_from_node(root, pattern, 0, u'', matchlist)
+        # Sort results in ascending lexicographic order
+        matchlist.sort(key = lambda x: Icelandic.sortkey(x))
+        return matchlist
+
+    def _perm_from_node(self, node, matched, rack, permlist):
         """ Starting from a given node, navigate the graph using the word and an index into it """
         if (not rack) or (node is None):
             # Nothing more to do (we assume that a match has already been recorded)
@@ -151,9 +251,9 @@ class DawgDictionary:
         for prefix, nextnode in node.edges.items():
             if (prefix[0] in rack) or (u'?' in rack):
                 # This edge is a candidate: navigate through it
-                self._append_perm_from_edge(matched, rack, prefix, nextnode, permlist)
+                self._perm_from_edge(matched, rack, prefix, nextnode, permlist)
 
-    def _append_perm_from_edge(self, matched, rack, prefix, nextnode, permlist):
+    def _perm_from_edge(self, matched, rack, prefix, nextnode, permlist):
         """ There is a letter in the rack matching the prefix of this edge """
         lenp = len(prefix)
         j = 0
@@ -193,7 +293,7 @@ class DawgDictionary:
         if rack and (nextnode is not None):
             # Gone through the entire edge and still have rack letters left:
             # continue with the next node
-            self._append_perm_from_node(nextnode, matched, rack, permlist)
+            self._perm_from_node(nextnode, matched, rack, permlist)
 
     def find_permutations(self, rack):
         """ Returns a list of legal permutations of a rack of letters.
@@ -206,133 +306,16 @@ class DawgDictionary:
         if self._nodes is None:
             # No graph: no permutations
             return permlist
+        if not rack:
+            # No rack to permute
+            return permlist
         root = self._nodes[0] # Start at the root
         if root is None:
             # No root: no permutations
             return permlist
-        self._append_perm_from_node(root, u'', rack, permlist)
+        self._perm_from_node(root, u'', rack, permlist)
         # Sort in descending order by length of permutation
         # and within that, in ascending lexicographic order
         permlist.sort(key = lambda x: (-len(x), Icelandic.sortkey(x)))
         return permlist
-
-import time
-
-class DawgTester:
-
-    def __init__(self):
-        self._dawg = None
-
-    def _test(self, word):
-        print(u"\"{0}\" is {1}found".format(word, u"" if self._dawg.find(word) else u"not "))
-
-    def _test_true(self, word):
-        if not self._dawg.find(word):
-            print(u"Error: \"{0}\" was not found".format(word))
-
-    def _test_false(self, word):
-        if self._dawg.find(word):
-            print(u"Error: \"{0}\" was found".format(word))
-
-    def run(self, fname, relpath):
-        """ Load a DawgDictionary and test its functionality """
-
-        print("DawgDictionary tester")
-        print("Author: Vilhjalmur Thorsteinsson")
-        print
-
-        self._dawg = DawgDictionary()
-        fpath = os.path.abspath(os.path.join(relpath, fname + ".text.dawg"))
-        t0 = time.time()
-        self._dawg.load(fpath)
-        t1 = time.time()
-
-        print("DAWG loaded in {0:.2f} seconds".format(t1 - t0))
-        print("Checking a set of random words:")
-        self._test_true(u"abbadísarinnar")
-        self._test_true(u"absintufyllirí")
-        self._test_false(u"absolútt")
-        self._test_true(u"aborri")
-        self._test_false(u"abs")
-        self._test_true(u"halló")
-        self._test_true(u"hraðskákmótin")
-        # self._test_true(u"jólahraðskákmótið") # Longer than 15 letters
-        self._test_true(u"nafnskírteinið")
-        self._test_false(u"abstraktmálarið")
-        self._test_true(u"abstraktmálari")
-        self._test_false(u"abstraktmálar")
-        self._test_false(u"abstraktmála")
-        self._test_true(u"prófun")
-        self._test_false(u"")
-        self._test_false(u"abo550")
-
-        # All two-letter words accepted by the Icelandic Scrabble(tm)-like game
-        # at ordaleikur.appspot.com
-        smallwords = [
-            u"að", u"af", u"ak", u"al", u"an", u"ar", u"as", u"at", u"ax",
-            u"áa", u"áð", u"ái", u"ál", u"ám", u"án", u"ár", u"ás", u"át",
-            u"bí", u"bú", u"bý", u"bæ",
-            u"dá", u"do", u"dó", u"dý",
-            u"eð", u"ef", u"eg", u"ei", u"ek", u"el", u"em", u"en", u"er", u"et", u"ex", u"ey",
-            u"ég", u"él", u"ét",
-            u"fa", u"fá", u"fé", u"fæ",
-            u"gá",
-            u"ha", u"há", u"hí", u"hó", u"hý", u"hæ",
-            u"ið", u"il",
-            u"íð", u"íl", u"ís",
-            u"já", u"jó", u"jú",
-            u"ká", u"ku", u"kú",
-            u"la", u"lá", u"lé", u"ló", u"lý", u"læ",
-            u"má", u"mi", u"mó", u"mý",
-            u"ná", u"né", u"nó", u"nú", u"ný", u"næ",
-            u"of", u"og", u"ok", u"op", u"or",
-            u"óa", u"óð", u"óf", u"ói", u"ók", u"ól", u"óm", u"ón", u"óp", u"ós", u"óx",
-            u"pí", u"pú",
-            u"rá", u"re", u"ré", u"rí", u"ró", u"rú", u"rý", u"ræ",
-            u"sá", u"sé", u"sí", u"so", u"sú", u"sý", u"sæ",
-            u"tá", u"te", u"té", u"ti", u"tí", u"tó", u"tý",
-            u"um", u"un",
-            u"úa", u"úð", u"úf", u"úi", u"úr", u"út",
-            u"vá", u"vé", u"ví", u"vó",
-            u"yl", u"ym", u"yr", u"ys",
-            u"ýf", u"ýg", u"ýi", u"ýk", u"ýl", u"ýr", u"ýs", u"ýt",
-            u"þá", u"þó", u"þú", u"þý",
-            u"æð", u"æf", u"æg", u"æi", u"æl", u"æp", u"ær", u"æs", u"æt",
-            u"öl", u"ör", u"ös", u"öt", u"öx"]
-
-        print("Checking small words:")
-
-        # Check all possible two-letter combinations, allowing only those in the list
-        for first in Icelandic.order:
-            for second in Icelandic.order:
-                word = first + second
-                if word in smallwords:
-                    self._test_true(word)
-                else:
-                    self._test_false(word)
-
-        print("Finding permutations:")
-        t0 = time.time()
-        word = u"einstök"
-        permlist = self._dawg.find_permutations(word)
-        t1 = time.time()
-        print(u"Permutations of \"{0}\":".format(word))
-        cnt = 0
-        for word in permlist:
-            print(u"\"{0}\"".format(word)),
-            cnt += 1
-            if cnt % 6 == 0:
-                print
-        print
-        print(u"{0} permutations found in {1:.2f} seconds".format(cnt, t1 - t0))
-        print
-        print(u"Test finished")
-
-        self._dawg = None
-
-
-def test():
-    # Test navivation in the DAWG
-    dt = DawgTester()
-    dt.run("ordalisti", "resources")
 
