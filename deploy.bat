db79a7b3858c7acb635c78d1eac9986c316480dc
Vilhjalmur Thorsteinsson <vt@extrada.com>
2021-01-15 18:21:38 +0000

    Updated word lists to conform with ksnid version of Netskrafl

--- deploy.bat
+++ deploy.bat
@@ -3,7 +3,6 @@ ECHO Deploy an update to App Server
 ECHO *** Run me from the Google Cloud SDK Shell! ***
 set GOOGLE_APPLICATION_CREDENTIALS=resources\skraflhjalp-d20c6ea64ce2.json
 
-REM gcloud beta app deploy --version=python3 --no-promote --project=skraflhjalp app.yaml
-gcloud beta app deploy --version=python3 --promote --project=skraflhjalp app.yaml
+gcloud beta app deploy --version=ksnid --no-promote --project=skraflhjalp app.yaml
 
 ECHO Default module deployment completed
